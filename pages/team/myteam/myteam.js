a94d787f674a6511429bcd1ad6a2e2716aff96e5
Airing <gx-deng@163.com>
2017-05-04 19:18:07 +0800

    初始化项目

---
+++ pages/team/myteam/myteam.js
@@ -0,0 +1,35 @@
+var app = getApp()
+
+Page({
+  data:{
+    userInfo: {}
+  },
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载   
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      that.setData({
+        userInfo: userInfo
+      })
+    })
+  },
+  onReady:function(){
+    // 生命周期函数--监听页面初次渲染完成
+    
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+    
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
+    }
+  }
+})
